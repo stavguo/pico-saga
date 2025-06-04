0588affd8e41a9f6b4ba0e5b3812de2af9ca7a70
stavguo <dmellogu@umich.edu>
2025-06-04 16:21:45 -0400

    Made camera movement smoother

--- src/camera.lua
+++ src/camera.lua
@@ -1,7 +1,7 @@
 function update_camera(cursor_idx)
     cursor_pos = indextovec(cursor_idx)
     -- Define deadzone boundaries (in tiles)
-    local dl, dr, dt, db, cam_x, cam_y = 4, 11, 4, 11, peek2(0x5f28), peek2(0x5f2a)
+    local dl, dr, dt, db, cam_x, cam_y = 4, 11, 4, 11, (peek2(0x5f28) \ 8) * 8, (peek2(0x5f2a) \ 8) * 8
     -- Calculate cursor position in screen space
     local csx, csy = cursor_pos[1] - (cam_x \ 8), cursor_pos[2] - (cam_y \ 8)
     -- Move camera if cursor is outside deadzone
@@ -11,8 +11,18 @@ function update_camera(cursor_idx)
     if (csy > db) cam_y = (cursor_pos[2] - db) * 8
     -- Clamp camera to map boundaries
     cam_x, cam_y = mid(0, cam_x, 128), mid(0, cam_y, 128)
-    -- Update camera position
-    camera(cam_x, cam_y)
+    local current_x, current_y, xx, yy = peek2(0x5f28), peek2(0x5f2a), 0, 0
+    if current_x < cam_x then
+        xx+=2
+    elseif current_x > cam_x then
+        xx-=2
+    end
+    if current_y < cam_y then
+        yy+=2
+    elseif current_y > cam_y then
+        yy-=2
+    end
+    camera(current_x + xx, current_y + yy)
 end
 
 function update_cursor(cursor, mw, mh)
